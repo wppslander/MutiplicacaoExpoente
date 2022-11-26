5df01fb8649ee20e5257e3e36a7d6d30a67fb66b
Wppslander <wppslander@gmail.com>
2022-11-26 17:38:08 -0300

    Arrumei Multiplicação

--- ListaLDE/src/listalde/ListaLDE.java
+++ ListaLDE/src/listalde/ListaLDE.java
@@ -6,64 +6,87 @@ public class ListaLDE {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char letra, opcao;
+        char opcao;
         LDE l1, l2, l3;
         l1 = new LDE();
         l2 = new LDE();
         l3 = new LDE();
+        /*
+        l1.inserir(2, 2);
+        l1.inserir(0, 7);
+        l1.inserir(0, 3);
+        l1.mostrar("Funcao1:");
+        
+        l2.inserir(2, 5);
+        l2.inserir(1, 3);
+        l2.inserir(0, 2);
+        l2.mostrar("Funcao 2:");
+        
+        l3 = l3.multiplicar(l1, l2);
+        l3.mostrar("Teste:");
+        
+        int exp;
+        double coef;
+        */
         
         do{
-            System.out.println("\nOpção ((1-inserir Elementos na função 1// 2-inserir elementos na função 2"
-                            + "\n// M-mostrar todas funções// *-Para multiplicar as duas funções// '-'-Para subtrair as duas funçõess// + Para somar as duas funções//F-fim): " );
+            
+            System.out.println("\nOpção ((1-inserir Elementos na função 1// 2-inserir elementos na função 2 // M-mostrar todas funções// *-Para multiplicar as duas funções// '-'-Para subtrair as duas funçõess// + Para somar as duas funções//F-fim): " );
             opcao = sc.nextLine().charAt(0);
             int exp;
             double coef;
             switch( opcao ){
-                case '1' -> 
+                case '1': 
                 {
-                    System.out.println("Função 1: Digite o expoente:");
+                    System.out.print("Função 1: Digite o expoente:");
                     exp = sc.nextInt();
-                    System.out.println("Função 1: Digite o coeficiente:");
+                    System.out.print("Função 1: Digite o coeficiente:");
                     coef = sc.nextDouble();
                     l1.inserir(exp, coef);
+                    break;
                 }
-                case '2' -> 
+                case '2': 
                 {
                     System.out.println("Função 2: Digite o expoente:");
                     exp = sc.nextInt();
                     System.out.println("Função 2: Digite o coeficiente:");
                     coef = sc.nextDouble();
                     l2.inserir(exp, coef);
+                    break;
                 }
-                case 'M', 'm' -> 
+                case 'M', 'm':
                 {
                     l1.mostrar("Função 1: ");
                     System.out.println("");
                     l2.mostrar("Função 2: ");
                     System.out.println("");
                     l3.mostrar("Função 3: ");
+                    break;
                 }
-                case '*' -> 
+                case '*': 
                 {
                     l3.multiplicar(l1, l2);
                     l3.mostrar("Multiplicação: ");
+                    break;
                 }
-                case '-' -> 
+                case '-':
                 {
                     l3.subtrairLista(l1, l2);
                     l3.mostrar("Subtração: ");
+                    break;
                 }
-                case '+' -> 
+                case '+':
                 {
                     l3.somarLista(l1, l2);
                     l3.mostrar("Soma: ");
+                    break;
                 }
-                default -> System.out.println("Opção invalida.");
+                
 
             }
-            
         }while( opcao != 'F' && opcao != 'f' );
-        System.out.println();
+
+        System.out.println("Você saiu");
 
     }
     
